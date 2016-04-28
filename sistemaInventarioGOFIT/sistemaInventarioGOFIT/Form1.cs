9c801127a4e06cb7873e9d4bd5378e529a671020
grettelUlate <gre061194@hotmail.com>
2016-04-28 17:57:21 -0600

    modified:   sistemaInventarioGOFIT/sistemaInventarioGOFIT.v12.suo 	modified:   sistemaInventarioGOFIT/sistemaInventarioGOFIT/Form1.Designer.cs 	modified:   sistemaInventarioGOFIT/sistemaInventarioGOFIT/Form1.cs 	new file:   sistemaInventarioGOFIT/sistemaInventarioGOFIT/Form1.resx 	modified:   sistemaInventarioGOFIT/sistemaInventarioGOFIT/Program.cs

--- sistemaInventarioGOFIT/sistemaInventarioGOFIT/Form1.cs
+++ sistemaInventarioGOFIT/sistemaInventarioGOFIT/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace sistemaInventarioGOFIT
 {
-    public partial class Form1 : Form
+    public partial class Interfaz : Form
     {
-        public Form1()
+        public Interfaz()
         {
             InitializeComponent();
         }
